1b56bf07aec76030d4e649c6823e70ae56539c26
cmrd-k <phil_krue@outlook.at>
2024-02-28 01:52:16 +0100

    adds create-recipe interface

--- src/app/pages/view-recipe/view-recipe.page.ts
+++ src/app/pages/view-recipe/view-recipe.page.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Ingredients } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-view-recipe',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './view-recipe.page.html',
   styleUrls: ['./view-recipe.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ViewRecipePage implements OnInit {
-
+  ingredients: Ingredients[] =[{name: "Tomaten",amount: 110, unit: "g"}];
   constructor() { }
 
   ngOnInit() {
   }
-
+  addIngredient(){
+    let blankIngredient:Ingredients = {name: "", amount: 0, unit: ""};
+    this.ingredients.push(blankIngredient);
+    console.log(this.ingredients);
+  }
+  removeIngredient(id: number){
+    this.ingredients.splice(id, 1)
+  }
 }
